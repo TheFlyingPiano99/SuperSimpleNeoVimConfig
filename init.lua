89a5508d46a37cd22d20c1ce3feab4b7b530949f
TheFlyingPiano99 <simonzolika1999@gmail.com>
2024-08-06 14:38:04 +0200

    Added yaml support

--- init.lua
+++ init.lua
@@ -56,7 +56,7 @@ require('lspconfig').ruff.setup({
 require'nvim-treesitter.configs'.setup{
   ensure_installed = {
     "c", "cpp", "lua", "vim", "vimdoc", "javascript", "html",
-    "python", "toml", "markdown", "json", "xml"
+    "python", "toml", "markdown", "json", "xml", "yaml"
   },
   highlight={enable=true}
 }
