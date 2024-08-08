0e62aa2e802b7315b846f8baa1ccbb11a342bfdc
TheFlyingPiano99 <simonzolika1999@gmail.com>
2024-08-08 17:22:51 +0200

    Moved lsp config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,30 +32,21 @@ require("lazy").setup({
     {
       "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
       lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-      dependencies = {
-        -- main one
-        { "ms-jpq/coq_nvim", branch = "coq" },
-
-        -- 9000+ Snippets
-        { "ms-jpq/coq.artifacts", branch = "artifacts" },
-
-        -- lua & third party sources -- See https://github.com/ms-jpq/coq.thirdparty
-        -- Need to **configure separately**
-        { 'ms-jpq/coq.thirdparty', branch = "3p" }
-        -- - shell repl
-        -- - nvim lua api
-        -- - scientific calculator
-        -- - comment banner
-        -- - etc
-      },
-      init = function()
-        vim.g.coq_settings = {
-            auto_start = true, -- if you want to start COQ at startup
-            -- Your COQ settings here
-        }
-      end,
       config = function()
-        -- Your LSP settings here
+        -- Python:
+        -- IMPORTANT: Ruff v5.3 (or higher version) must be installed separately!
+        require('lspconfig').ruff.setup({
+          init_options = {
+            settings = {
+              -- Ruff language server settings go here
+              configurationPreference = "filesystemFirst"
+            }
+          }
+        })
+
+        -- C/C++:
+        -- IMPORTANT: clangd must be installed separately!
+        require'lspconfig'.clangd.setup{}
       end,
     },
     {
