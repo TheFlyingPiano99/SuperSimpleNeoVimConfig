987905c33405b4a6a7e43200837d7f532e84be95
TheFlyingPiano99 <simonzolika1999@gmail.com>
2024-09-03 19:03:33 +0200

    Reindented lazy.lua

--- init.lua
+++ init.lua
@@ -15,24 +15,31 @@ set.hidden = true           -- Allows you to switch between buffers without need
 set.swapfile = false        -- Swap file creation
 set.backup = false          -- Backup file creation when saving
 set.hlsearch = true         -- Highlight search results
+set.incsearch = true        -- Incremental search results
 set.wrap = false            -- Wrap long lines
 set.mouse = 'a'             -- Enable mouse usage in all modes
 set.termguicolors = true    -- Enable 24-bit RGB color
-set.scrolloff = 2           -- the minimum number of screen lines to keep above and below the cursor when scrolling through a buffer 
+set.scrolloff = 4           -- the minimum number of screen lines to keep above and below the cursor when scrolling through a buffer 
 set.number = true           -- Enable line numbering
 set.relativenumber = false  -- Absolute line numbering
 
-set.tabstop = 2             -- The number of spaces that a tab character represents. This setting affects how tabs are displayed in the editor 
-set.shiftwidth = 2          -- The number of spaces to use for each step of indentation 
-set.softtabstop = 2         -- The number of spaces that a tab character represents when editing text
+set.tabstop = 4             -- The number of spaces that a tab character represents. This setting affects how tabs are displayed in the editor 
+set.shiftwidth = 4          -- The number of spaces to use for each step of indentation 
+set.softtabstop = 4         -- The number of spaces that a tab character represents when editing text
 set.expandtab = true        -- Whether the tab key inserts spaces instead of a tab character.
                             -- When expandtab is enabled, pressing the tab key will insert a number of spaces equivalent to the tabstop value 
+set.smartindent = true      -- Syntax/style reactive indenting
+
+vim.opt.updatetime = 50     -- Fast update time
 
 vim.g.netrw_winsize = 20    -- This option controls the size of the window used by netrw when it opens 
-vim.g.netrw_banner = 0      -- Set netrw banner (disable)
+vim.g.netrw_banner = 1      -- Set netrw banner
+vim.g.mapleader = " "       -- Leader key is space
+
+
 
 ---------------------------------------------
 
 -- LAZY
 require("config.lazy")
-
+require("config.remap")
--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out, "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,81 +23,85 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    {
-      'nvim-treesitter/nvim-treesitter',
-      lazy = false,
-      config = function()
+    spec = {
+        {
+            'nvim-treesitter/nvim-treesitter',
+            lazy = false,
+            config = function()
+                local function install_tree_sitter_cli()
+                    local handle = io.popen("tree-sitter --version")
+                    local result = handle:read("*a")
+                    handle:close()
 
-        local function install_tree_sitter_cli()
-          local handle = io.popen("tree-sitter --version")
-          local result = handle:read("*a")
-          handle:close()
+                    if result == "" then
+                        print("tree-sitter-cli not found. Installing...")
 
-          if result == "" then
-            print("tree-sitter-cli not found. Installing...")
+                        -- Define the installation command for your system
+                        -- For example, using npm:
+                        local install_cmd = "npm install -g tree-sitter-cli"
 
-            -- Define the installation command for your system
-            -- For example, using npm:
-            local install_cmd = "npm install -g tree-sitter-cli"
+                        -- Run the command
+                        os.execute(install_cmd)
 
-            -- Run the command
-            os.execute(install_cmd)
+                        -- Verify installation
+                        handle = io.popen("tree-sitter --version")
+                        result = handle:read("*a")
+                        handle:close()
 
-            -- Verify installation
-            handle = io.popen("tree-sitter --version")
-            result = handle:read("*a")
-            handle:close()
+                        if result == "" then
+                            print("Failed to install tree-sitter-cli.")
+                        else
+                            print("tree-sitter-cli installed successfully.")
+                        end
+                    else
+                        --print("tree-sitter-cli is already installed: " .. result)
+                    end
+                end
 
-            if result == "" then
-              print("Failed to install tree-sitter-cli.")
-            else
-              print("tree-sitter-cli installed successfully.")
+                -- Call the function during startup
+                install_tree_sitter_cli()
+
+                require'nvim-treesitter.configs'.setup{
+                    ensure_installed = {
+                        "c", "cpp", "python", "lua", "vim", "vimdoc", "javascript", "html",
+                        "css", "markdown", "json", "xml", "yaml", "toml", "latex"
+                    },
+                    highlight = {enable=true}
+                }
             end
-          else
-            --print("tree-sitter-cli is already installed: " .. result)
-          end
-        end
+        },
+        {
+            "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
+            lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
+            config = function()
+                local lsp = require('lspconfig')
+                -- Python:
+                -- IMPORTANT: Ruff v5.3 (or higher version) must be installed separately!
+                lsp.ruff.setup{
+                    -- Ruff language server settings go here
+                    configurationPreference = "filesystemFirst"
+                }
 
-        -- Call the function during startup
-        install_tree_sitter_cli()
-
-        require'nvim-treesitter.configs'.setup{
-          ensure_installed = {
-            "c", "cpp", "python", "lua", "vim", "vimdoc", "javascript", "html",
-            "css", "markdown", "json", "xml", "yaml", "toml", "latex"
-          },
-          highlight = {enable=true}
-        }
-      end
+                -- C/C++:
+                -- IMPORTANT: clangd must be installed separately!
+                lsp.clangd.setup{}
+            end,
+        },
+        {
+            'nvim-telescope/telescope.nvim',
+            branch = '0.1.x',
+            dependencies = { 'nvim-lua/plenary.nvim' }
+        },
+        { 
+            "rose-pine/neovim",
+            name = "rose-pine",
+            config = function()
+                vim.cmd("colorscheme rose-pine")
+            end
+        },
     },
-    {
-      "neovim/nvim-lspconfig", -- REQUIRED: for native Neovim LSP integration
-      lazy = false, -- REQUIRED: tell lazy.nvim to start this plugin at startup
-      config = function()
-        local lsp = require('lspconfig')
-        -- Python:
-        -- IMPORTANT: Ruff v5.3 (or higher version) must be installed separately!
-        lsp.ruff.setup{
-          -- Ruff language server settings go here
-          configurationPreference = "filesystemFirst"
-        }
-
-        -- C/C++:
-        -- IMPORTANT: clangd must be installed separately!
-        lsp.clangd.setup{}
-      end,
-    },
-    { 
-      "rose-pine/neovim",
-      name = "rose-pine",
-      config = function()
-        vim.cmd("colorscheme rose-pine")
-      end
-    },
-  },
-  -- automatically check for plugin updates:
-  checker = { enabled = true },
-  -- Configure any other settings here. See the documentation for more details.
+    -- automatically check for plugin updates:
+    checker = { enabled = true },
+    -- Configure any other settings here. See the documentation for more details.
 })
 
